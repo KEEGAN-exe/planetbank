a00285788cf0770f687376ad0b835ec2f573e17b
Keegan <juan789huarcaya@gmail.com>
2023-11-02 16:41:10 -0500

    configurando el proyecto

---
+++ src/main/java/com/planetbank/PlanetbankApplication.java
@@ -0,0 +1,13 @@
+package com.planetbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetbankApplication.class, args);
+	}
+
+}
